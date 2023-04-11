35c07b604aace8319e9292869f5597288e93f27a
Rami <74742680+Rami2789@users.noreply.github.com>
2023-04-11 15:21:40 +0200

    created connection with database

---
+++ backend/DbConfig.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The DbConfig class represents a database configuration and provides a method to establish a connection to a MySQL database.
+ */
+class DbConfig{
+
+    /**
+     * Establishes a connection to a MySQL database.
+     *
+     * @return PDO Returns a PDO object representing the database connection.
+     *
+     * @throws PDOException If an error occurs while connecting to the database.
+     */
+    public function connect(){
+        try{
+            $conn = new PDO("mysql:host=localhost;dbname=kapsalontest", 'root', '');
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+}
+?>
