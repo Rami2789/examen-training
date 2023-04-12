5fb98ec7fe32f560e40b54b38f1066b3a0a5ccbf
Rami <74742680+Rami2789@users.noreply.github.com>
2023-04-12 22:56:50 +0200

    Created the profile page. you can see the user info and can update them too

---
+++ backend/profile.php
@@ -0,0 +1,67 @@
+<?php
+require_once 'class/user.php';
+
+$update = new User();
+
+if (isset($_POST['update'])) {
+
+    $update->userUpdate($_POST);
+
+}
+
+session_start();
+if(!$_SESSION['ingelogd']|| !$_SESSION['ingelogd']){
+    header("Location: ../login.php");
+}
+
+$user = $update->getUpdate($_SESSION['email']);
+
+
+?>
+
+        <section class="profile">
+        <?php
+           foreach ($update->getUpdate($_SESSION['email']) as $users) {
+        ?>
+            <form class="item-info" method="post">
+                <article  class="item-info1">
+                    <p>voornaam</p>
+                    <input type="text" value='<?php echo $users->voornaam;?>' name="voornaam" placeholder="voornaam">
+                    <p>Tussenvoegsel</p>
+                    <input type="text" value='<?php echo $users->tussenvoegsel;?>' name="tussenvoegsel" placeholder="Tussenvoegsel">
+                    <p>Achternaam</p>
+                    <input type="text" value='<?php echo $users->achternaam;?>' name="achternaam" placeholder="Achternaam">
+                </article>
+                <article class="item-info2">
+                    <p>Email</p>
+                    <input type="text" value='<?php echo $users->email;?>' name="email" placeholder="Email">
+                    <p>Wachtwoord</p>
+                    <input type="password" name="password" placeholder="Password">
+                    <input type="submit" name="update" value="Update">
+                </article>
+            </form>
+            <?php } ?>
+        </section>
+
+
+        <section class="profile">
+            <form class="item-info" method="post">
+                <article  class="item-info1">
+                    <p>voornaam</p>
+                    <input type="text" value='<?php echo $user[0]->voornaam;?>' name="voornaam" placeholder="voornaam">
+                    <p>Tussenvoegsel</p>
+                    <input type="text" value='<?php echo $user[0]->tussenvoegsel;?>' name="tussenvoegsel" placeholder="Tussenvoegsel">
+                    <p>Achternaam</p>
+                    <input type="text" value='<?php echo $user[0]->achternaam;?>' name="achternaam" placeholder="Achternaam">
+                </article>
+                <article class="item-info2">
+                    <p>Email</p>
+                    <input type="text" value='<?php echo $user[0]->email;?>' name="email" placeholder="Email">
+                    <p>Wachtwoord</p>
+                    <input type="password" name="password" placeholder="Password">
+                    <input type="submit" name="update" value="Update">
+                </article>
+                
+                
+            </form>
+        </section>
