418e0d469c92aadc5a8624d1cf462f0ce5803de9
Rami <74742680+Rami2789@users.noreply.github.com>
2023-04-12 20:25:19 +0200

    Made a login page so the user can login

---
+++ login.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Includes the necessary files and creates a User object.
+ */
+require_once 'backend/user.php';
+$user = new User();
+
+/**
+ * Checks if the login form has been submitted and calls the login method of the User object.
+ *
+ * @param array $_POST The POST request data sent from the login form.
+ *
+ * @return string Returns the output of the login method.
+ */
+if (isset($_POST['login'])) {
+  echo $user->login($_POST);
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/login-register.css">
+    <link rel="icon" type="image/x-icon" href="img/logo1.png">
+  </head>
+  <body>
+            <form class="box" method="post">
+                <!-- <img src="img/logo.png" width="200" alt="Logo"> -->
+                <h1>Login</h1>
+                <input type="email" name="email" placeholder="Email" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <input type="submit" name="login" value="Login">
+                <p class="login">
+                  Nieuw? <a href="register.php">Registreer hier</a>
+                </p>
+            </form>
+  </body>
+</html>
