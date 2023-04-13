b7a623a543856c71aae7964ec25b7a968f1f2ff6
Rami <74742680+Rami2789@users.noreply.github.com>
2023-04-13 16:49:19 +0200

    created a form so that a not logged in user can comment on a post and the comment will view under the form

--- backend/postinfo.php
+++ backend/postinfo.php
@@ -1,12 +1,15 @@
 <?php
 require_once 'partial/header.php';
-require_once 'class/Post.php';
+require_once 'class/post.php';
+$post = new Post();
+if(isset($_POST['create'])){
+	echo $post->comment($_POST);
+}
 ?>
 
 
 
 <?php
-    $post = new Post();
     foreach ($post->getPostID($_GET["posts"]) as $post) { ?>
         <article>
             <p>Author: <?php echo $post->gebruikersnaam; ?></p>
@@ -18,3 +21,26 @@ require_once 'class/Post.php';
         </article>
     <?php }
 ?>
+
+
+    <section class="form">
+	    	<form method="post">
+            <input type="text" name="naam" placeholder="Naam" required>
+            <input type="text" name="message" placeholder="Message" required>
+            <input type="number" name="postId" value="<?php echo $_GET['posts'] ?>" readonly hidden>
+            <input type="submit" name="create" value="Comment">
+	    	</form>
+    </section>
+
+    <main>
+      <section class="content">
+        <?php 
+            $post = new Post();
+            foreach ($post->getComments($_GET["posts"]) as $postData) { ?>
+            <article class="post">
+                <?php echo $postData->author . "<br>"; ?>
+                <?php echo $postData->message . "<br>"; ?>
+            </article>
+        <?php } ?>
+      </section>
+    </main>
