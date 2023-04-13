4c7242327bbd1b2d945d2d3571c84d8f1d696779
Rami <74742680+Rami2789@users.noreply.github.com>
2023-04-13 15:03:06 +0200

    added comments to the javascript function

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,9 @@
 
+/**
+ * Confirm account deletion with user before proceeding
+ * 
+ * @returns boolean true if user confirms deletion, false otherwise
+ */
 function confirmDelete() {
     var result = confirm("Are you sure you want to delete your account?");
     if (result) {
