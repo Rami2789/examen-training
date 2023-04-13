3353aa764511de9a6a4f5c5c660a418e4961c42f
Rami <74742680+Rami2789@users.noreply.github.com>
2023-04-13 16:33:00 +0200

    Changed the file so that the logged in user can see all posts that other users have posted

--- backend/allposts.php
+++ backend/allposts.php
@@ -9,18 +9,12 @@ $post = new Post();
     <main>
       <section class="content">
           
-      <?php
-        foreach ($post->getPostFromUser($_SESSION['id']) as $postData) {
-            ?>
-        <article class="post">
+        <?php foreach ($post->getPost() as $postData) { ?>
+          <article class="post">
             <h3><a href="postinfo.php?posts=<?php echo $postData->id ?>"><?php echo $postData->title; ?></a></h3>
-          <?php echo $postData->description . "<br>"; ?>
-          <?php echo $postData->body . "<br>"; ?>
-          <a href="postedit.php?posts=<?php echo $postData->id ?>">Edit</a>
-          <a href="postedit.php?posts=<?php echo $postData->id ?>&delete=true" onclick="return confirmDelete();">Delete</a>
-        </article>
+            <?php echo $postData->description . "<br>"; ?>
+            <?php echo $postData->body . "<br>"; ?>
+          </article>
         <?php } ?>
       </section>
     </main>
-
-<script src="../js/posts.js"></script>
