9aca6210f5026dc81fc3f2124df10a51222398a6
Rami <74742680+Rami2789@users.noreply.github.com>
2023-04-13 16:15:02 +0200

    created a file to edit the post and from there you can also delete the post

---
+++ backend/postedit.php
@@ -0,0 +1,38 @@
+<?php
+require_once 'partial/header.php';
+require_once 'class/Post.php';
+
+$post = new Post();
+if (isset($_POST['update'])) {
+    $post->editpost($_POST, $_GET['posts']);
+}
+
+if (isset($_POST['delete']) || isset($_GET['delete'])) {
+    $post->deletePost($_POST, $_GET['posts']);
+    header("Location: allposts.php");
+}
+?>
+
+
+
+<?php
+
+    foreach ($post->getPostID($_GET["posts"]) as $post) { ?>
+        <article>
+            <form method="post">
+            <input type="text" name="title" value="<?php echo $post->title; ?>">
+            <input type="text" name="description" value="<?php echo $post->description; ?>">
+            <input type="text" name="body" value="<?php echo $post->body; ?>">
+            <input type="submit" name="update" value="Update">
+            <input type="submit" name="delete" value="delete">
+            </form>
+        </article>
+    <?php }
+?>
+
+
+
+<?php
+require_once 'partial/footer.php';
+?>
+
